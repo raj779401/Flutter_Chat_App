15667d144a3869af666355b046459822b95b319e
raj779401 <raj779401@gmail.com>
2024-04-05 15:19:23 +0530

    bug fixed in authentication screen

--- chat_app/lib/screens/authentication_screen.dart
+++ chat_app/lib/screens/authentication_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AuthenticationScreen extends StatefulWidget {
-  const AuthenticationScreen({super.key});
+  const AuthenticationScreen({Key? key}) : super(key: key);
 
   @override
   State<AuthenticationScreen> createState() => _AuthenticationScreenState();
@@ -10,21 +10,21 @@ class AuthenticationScreen extends StatefulWidget {
 class _AuthenticationScreenState extends State<AuthenticationScreen> {
   final _formKey = GlobalKey<FormState>();
   var _enteredEmail = '';
-  var _eneteredPassword = '';
+  var _enteredPassword = '';
+  bool _isLogin = true;
+
   void _submitForm() {
     final isValid = _formKey.currentState != null
         ? _formKey.currentState!.validate()
         : false;
-    if (!isValid) {
-      if (_formKey.currentState != null) {
-        _formKey.currentState!.save();
-        print(_eneteredPassword);
-        print(_enteredEmail);
-      }
+    if (isValid) {
+      // Form is valid, you can proceed with form submission or other actions.
+      _formKey.currentState!.save();
+      print(_enteredPassword);
+      print(_enteredEmail);
     }
   }
 
-  bool _isLogin = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,12 +37,12 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
               Container(
                 margin: EdgeInsets.only(
                   top: 30,
-                  right: 20,
                   left: 20,
+                  right: 20,
                   bottom: 20,
                 ),
                 width: 200,
-                child: Image.asset('assets/images/chat.png'),
+                child: Image.asset("assets/images/chat.png"),
               ),
               Card(
                 margin: EdgeInsets.all(20),
@@ -50,49 +50,60 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
                   child: Padding(
                     padding: EdgeInsets.all(16),
                     child: Form(
+                      key: _formKey, // Add this line
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
                           TextFormField(
-                            decoration:
-                                InputDecoration(labelText: 'Email Address'),
+                            decoration: InputDecoration(
+                              labelText: 'Email Address',
+                            ),
                             keyboardType: TextInputType.emailAddress,
                             autocorrect: false,
                             textCapitalization: TextCapitalization.none,
                             validator: (value) {
                               if (value == null ||
                                   value.trim().isEmpty ||
-                                  value.contains("@")) {
-                                return 'Please enter a valid email address';
+                                  !value.contains('@')) {
+                                return "Enter a valid Email Address";
                               }
                               return null;
                             },
-                            onSaved: (value) {
-                              _enteredEmail = value!;
+                            onSaved: (newValue) {
+                              _enteredEmail = newValue!;
                             },
                           ),
-                          SizedBox(height: 10),
+                          SizedBox(
+                            height: 10,
+                          ),
                           TextFormField(
-                            decoration: InputDecoration(labelText: 'Password'),
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                            ),
                             obscureText: true,
                             validator: (value) {
-                              if (value == null || value.length < 6) {
-                                return 'Password must be at least 6 characters long';
+                              if (value == null ||
+                                  value.trim().isEmpty ||
+                                  value.length < 7) {
+                                return "Password must be at least 7 characters long";
                               }
                               return null;
                             },
-                            onSaved: (value) {
-                              _eneteredPassword = value!;
+                            onSaved: (newValue) {
+                              _enteredPassword = newValue!;
                             },
                           ),
-                          const SizedBox(height: 30),
+                          SizedBox(
+                            height: 10,
+                          ),
                           ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: Theme.of(context)
-                                    .colorScheme
-                                    .primaryContainer),
-                            onPressed: _submitForm,
+                            onPressed: _submitForm, // Change this line
                             child: Text(_isLogin ? 'Login' : 'SignUp'),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .primaryContainer,
+                            ),
                           ),
                           TextButton(
                             onPressed: () {
@@ -101,9 +112,9 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
                               });
                             },
                             child: Text(_isLogin
-                                ? 'Create an Account'
-                                : 'Already have an Account'),
-                          ),
+                                ? 'Create New Account'
+                                : 'I already have an account'),
+                          )
                         ],
                       ),
                     ),
