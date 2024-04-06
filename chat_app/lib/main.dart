eb839a4d9e9bbebacc09089438c47076b59145bf
raj779401 <raj779401@gmail.com>
2024-04-06 11:16:18 +0530

    chat screen added

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app/screens/authentication_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -18,13 +19,22 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'FlutterChat',
-        theme: ThemeData().copyWith(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 63, 17, 177)),
-        ),
-        home: AuthenticationScreen());
+      debugShowCheckedModeBanner: false,
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 63, 17, 177)),
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
+          return const AuthenticationScreen();
+        },
+      ),
+    );
   }
 }
