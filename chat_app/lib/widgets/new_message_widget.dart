ef777efdd9a54e718c87312cb52c4e7f0c64cd8b
raj779401 <raj779401@gmail.com>
2024-04-06 16:39:00 +0530

    firestore features added

--- chat_app/lib/widgets/new_message_widget.dart
+++ chat_app/lib/widgets/new_message_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class NewMessageWidget extends StatefulWidget {
   const NewMessageWidget({super.key});
@@ -17,13 +19,31 @@ class _NewMessageWidgetState extends State<NewMessageWidget> {
     super.dispose();
   }
 
-  void _submitMessage() {
+  void _submitMessage() async {
     final enteredMessage = _messageController.text;
     if (enteredMessage.trim().isEmpty) {
       return;
     }
 
-    //send to firebase
+    final user = FirebaseAuth.instance.currentUser!;
+    final userData = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .get();
+    if (!userData.exists) {
+      // Handle the case where userData does not exist
+      print('User data does not exist!');
+      return;
+    }
+
+    FirebaseFirestore.instance.collection('chat').add({
+      'text': enteredMessage,
+      'createdAt': Timestamp.now(),
+      'userId': user.uid,
+      'username': userData.data()!['username'],
+      'userImage': userData.data()!['image_url'],
+    });
+    FocusScope.of(context).unfocus();
     _messageController.clear();
   }
 
