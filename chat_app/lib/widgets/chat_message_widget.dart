668f523cfc401d1d2d40b0e0e5ebabc4331ecd14
raj779401 <raj779401@gmail.com>
2024-04-06 17:52:17 +0530

    showing chat messages

--- chat_app/lib/widgets/chat_message_widget.dart
+++ chat_app/lib/widgets/chat_message_widget.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessageWidget extends StatelessWidget {
@@ -5,8 +6,32 @@ class ChatMessageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('No Messages found ..!'),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: false)
+          .snapshots(),
+      builder: (ctx, chatSnapshots) {
+        if (chatSnapshots.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
+          return Center(
+            child: Text('No Message Found ..!'),
+          );
+        }
+        if (chatSnapshots.hasError) {
+          return Center(
+            child: Text('Something went wrong'),
+          );
+        }
+
+        final loadedMessage = chatSnapshots.data!.docs;
+        return ListView.builder(
+            itemCount: loadedMessage.length,
+            itemBuilder: (ctx, index) =>
+                Text(loadedMessage[index].data()['text']));
+      },
     );
   }
 }
